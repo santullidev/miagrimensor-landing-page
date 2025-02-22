ee84af8449d0e66212b890ee347323ec0a034247
akash3444 <akashmoradiya3444@gmail.com>
2025-02-22 19:53:09 +0530

    feat: add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import FAQ from "@/components/faq";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import { Navbar } from "@/components/navbar";
+import Pricing from "@/components/pricing";
+import Testimonial from "@/components/testimonial";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FAQ />
+      <Testimonial />
+      <Pricing />
+      <Footer />
+    </>
+  );
 }
