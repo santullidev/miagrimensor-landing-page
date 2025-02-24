972c0bc55ee0aa658956df2df4b01466f8e02737
akash3444 <akashmoradiya3444@gmail.com>
2025-02-24 18:21:47 +0530

    fix: layout shift caused by accordion

--- components/faq.tsx
+++ components/faq.tsx
@@ -42,10 +42,7 @@ const faq = [
 
 const FAQ = () => {
   return (
-    <div
-      id="faq"
-      className="w-full max-w-screen-xl mx-auto py-12 xs:py-20 px-6"
-    >
+    <div id="faq" className="w-full max-w-screen-xl mx-auto py-8 xs:py-16 px-6">
       <h2 className="md:text-center text-3xl xs:text-4xl md:text-5xl !leading-[1.15] font-bold tracking-tighter">
         Frequently Asked Questions
       </h2>
@@ -53,34 +50,36 @@ const FAQ = () => {
         Quick answers to common questions about our products and services.
       </p>
 
-      <Accordion
-        type="single"
-        collapsible
-        className="mt-8 space-y-4 md:columns-2 gap-4"
-      >
-        {faq.map(({ question, answer }, index) => (
-          <AccordionItem
-            key={question}
-            value={`question-${index}`}
-            className="bg-accent py-1 px-4 rounded-xl border-none !mt-0 !mb-4 break-inside-avoid"
-          >
-            <AccordionPrimitive.Header className="flex">
-              <AccordionPrimitive.Trigger
-                className={cn(
-                  "flex flex-1 items-center justify-between py-4 font-semibold tracking-tight transition-all hover:underline [&[data-state=open]>svg]:rotate-45",
-                  "text-start text-lg"
-                )}
-              >
-                {question}
-                <PlusIcon className="h-5 w-5 shrink-0 text-muted-foreground transition-transform duration-200" />
-              </AccordionPrimitive.Trigger>
-            </AccordionPrimitive.Header>
-            <AccordionContent className="text-[15px]">
-              {answer}
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <div className="min-h-[550px] md:min-h-[320px] xl:min-h-[300px]">
+        <Accordion
+          type="single"
+          collapsible
+          className="mt-8 space-y-4 md:columns-2 gap-4"
+        >
+          {faq.map(({ question, answer }, index) => (
+            <AccordionItem
+              key={question}
+              value={`question-${index}`}
+              className="bg-accent py-1 px-4 rounded-xl border-none !mt-0 !mb-4 break-inside-avoid"
+            >
+              <AccordionPrimitive.Header className="flex">
+                <AccordionPrimitive.Trigger
+                  className={cn(
+                    "flex flex-1 items-center justify-between py-4 font-semibold tracking-tight transition-all hover:underline [&[data-state=open]>svg]:rotate-45",
+                    "text-start text-lg"
+                  )}
+                >
+                  {question}
+                  <PlusIcon className="h-5 w-5 shrink-0 text-muted-foreground transition-transform duration-200" />
+                </AccordionPrimitive.Trigger>
+              </AccordionPrimitive.Header>
+              <AccordionContent className="text-[15px]">
+                {answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
   );
 };
