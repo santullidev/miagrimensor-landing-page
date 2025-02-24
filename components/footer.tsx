728ed351ed71b60c5f964124bc0e8b992e24c546
akash3444 <akashmoradiya3444@gmail.com>
2025-02-24 18:05:42 +0530

    fix(ui): footer links alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -158,7 +158,7 @@ const footerSections = [
 const Footer = () => {
   return (
     <footer className="mt-12 xs:mt-20 dark bg-background border-t">
-      <div className="max-w-screen-xl mx-auto py-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-x-8 gap-y-10 px-6 xl:px-0">
+      <div className="max-w-screen-xl mx-auto py-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-x-8 gap-y-10 px-6">
         <div className="col-span-full xl:col-span-2">
           {/* Logo */}
           <svg
@@ -186,7 +186,7 @@ const Footer = () => {
         </div>
 
         {footerSections.map(({ title, links }) => (
-          <div key={title}>
+          <div key={title} className="xl:justify-self-end">
             <h6 className="font-semibold text-foreground">{title}</h6>
             <ul className="mt-6 space-y-4">
               {links.map(({ title, href }) => (
@@ -204,7 +204,7 @@ const Footer = () => {
         ))}
       </div>
       <Separator />
-      <div className="max-w-screen-xl mx-auto py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
+      <div className="max-w-screen-xl mx-auto py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6">
         {/* Copyright */}
         <span className="text-muted-foreground text-center xs:text-start">
           &copy; {new Date().getFullYear()}{" "}
